1f3da8d4e9eafa6d581311d0038ac1c128114ebe
Christian Clyde Decierdo <decierdocc1999@gmail.com>
2024-07-04 20:40:54 +0800

    implemented an expandable large FAB with extended FAB children

--- app/src/main/java/com/example/moodful/MainActivity.kt
+++ app/src/main/java/com/example/moodful/MainActivity.kt
@@ -4,6 +4,9 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.animation.AnimatedVisibility
+import androidx.compose.animation.fadeIn
+import androidx.compose.animation.fadeOut
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
@@ -13,15 +16,30 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.wrapContentHeight
+import androidx.compose.foundation.layout.wrapContentSize
 import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Close
+import androidx.compose.material.icons.filled.Create
+import androidx.compose.material.icons.filled.FavoriteBorder
+import androidx.compose.material.icons.filled.Menu
+import androidx.compose.material3.ExtendedFloatingActionButton
+import androidx.compose.material3.Icon
+import androidx.compose.material3.LargeFloatingActionButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.draw.shadow
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -50,19 +68,20 @@ fun FrontPage(modifier: Modifier = Modifier) {
         dynamicColor = false,
         darkTheme = false
     ) {
-        Scaffold{
-            innerPadding -> Surface(
+        Scaffold { innerPadding ->
+            Surface(
                 modifier = modifier
                     .fillMaxSize()
                     .padding(innerPadding),
-                color = MaterialTheme.colorScheme.surfaceDim
+                color = MaterialTheme.colorScheme.surface
             ) {
                 ConstraintLayout {
                     // TODO: Create refs here
                     val dateRef = createRef()
                     val timeRef = createRef()
+                    val floatingButtonRef = createRef()
                     DateToday(
-                        modifier = modifier
+                        modifier = Modifier
                             .constrainAs(dateRef) {
                                 start.linkTo(parent.start)
                                 end.linkTo(parent.end)
@@ -70,12 +89,19 @@ fun FrontPage(modifier: Modifier = Modifier) {
                             }
                     )
                     DateAndTimeDisplay(
-                        modifier = modifier
+                        modifier = Modifier
                             .constrainAs(timeRef) {
                                 top.linkTo(parent.top)
                                 bottom.linkTo(parent.bottom)
                                 start.linkTo(parent.start)
                                 end.linkTo(parent.end)
+                            }
+                    )
+                    HubButton(
+                        modifier = Modifier
+                            .constrainAs(floatingButtonRef) {
+                                bottom.linkTo(parent.bottom, margin = 18.dp)
+                                end.linkTo(parent.end, margin = 18.dp)
                             }
                     )
                 }
@@ -118,10 +144,9 @@ fun DateAndTimeDisplay(modifier: Modifier = Modifier) {
     ) {
         Surface(
             modifier = modifier
-                .size(290.dp) // Ensure it's a square
+                .size(280.dp) // Ensure it's a square
                 .clip(CircleShape),
             color = MaterialTheme.colorScheme.inversePrimary,
-            tonalElevation = 8.dp
         ) {
             Column(
                 horizontalAlignment = Alignment.CenterHorizontally,
@@ -158,6 +183,72 @@ fun DateAndTimeDisplay(modifier: Modifier = Modifier) {
                 )
             }
         }
+    }
+}
+
+
+@Composable
+fun HubButton(modifier: Modifier = Modifier) {
+    var expanded by remember { mutableStateOf(false) }
+
+    Box(
+        modifier = modifier
+            .wrapContentSize()
+            .padding(16.dp),
+        contentAlignment = Alignment.BottomEnd
+    ) {
+        Column(
+            horizontalAlignment = Alignment.End,
+            verticalArrangement = Arrangement.spacedBy(16.dp)
+        ) {
+            AnimatedVisibility(
+                visible = expanded,
+                enter = fadeIn(),
+                exit = fadeOut()
+            ) {
+                Column(
+                    horizontalAlignment = Alignment.End,
+                    verticalArrangement = Arrangement.spacedBy(16.dp)
+                ) {
+                    ExtendedFloatingActionButton(
+                        modifier = modifier.shadow(8.dp, RoundedCornerShape(16.dp)),
+                        text = { Text("Create new entry") },
+                        onClick = { /* Action 1 */ },
+                        icon = { Icon(Icons.Filled.Create, "Create") }
+                    )
+                    ExtendedFloatingActionButton(
+                        modifier = modifier.shadow(8.dp, RoundedCornerShape(16.dp)),
+                        text = { Text("Open diary") },
+                        onClick = { /* Action 2 */ },
+                        icon = { Icon(Icons.Filled.Menu, "Menu") }
+                    )
+                }
+            }
+
+            LargeFloatingActionButton(
+                onClick = { expanded = !expanded },
+                shape = CircleShape,
+                contentColor = MaterialTheme.colorScheme.surfaceContainerLow,
+                containerColor = MaterialTheme.colorScheme.tertiaryContainer
+            ) {
+                Icon(
+                    imageVector = if (expanded) Icons.Filled.Close else Icons.Filled.FavoriteBorder,
+                    contentDescription = "More actions"
+                )
+            }
+        }
+    }
+}
+
+
+@Preview(showBackground = true)
+@Composable
+fun HubButtonPreview() {
+    MoodfulTheme(
+        darkTheme = false,
+        dynamicColor = false
+    ) {
+        HubButton()
     }
 }
 
